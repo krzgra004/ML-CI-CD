797d6e1ba7379e0e496e2921433831c7e725b489
unknown <krzgra004@pbs.edu.pl>
2025-04-29 19:18:24 +0200

    forth commit

--- app.py
+++ app.py
@@ -8,12 +8,6 @@ from model import train_and_predict, get_accuracy
 
 app = FastAPI()
 
-X_train = np.array([[0.5], [14.0], [15.0], [28.0], [11.0], [8.0], [3.0], [-4.0], [6.0], [13.0], [21.0]])
-y_train = np.array([35.7, 55.9, 58.2, 81.9, 56.3, 48.9, 33.9, 21.8, 48.4, 60.4, 68.4])
-
-model = LinearRegression()
-model.fit(X_train, y_train)
-
 class InputData(BaseModel):
     value: float
 
